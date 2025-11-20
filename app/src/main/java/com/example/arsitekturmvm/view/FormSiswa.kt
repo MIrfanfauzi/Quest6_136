fb71e3a6b2be6aafef706483ea6fc57241d5dd23
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-11-20 16:48:22 +0700

    menambahkan topappbar menggunakan scaffold

--- app/src/main/java/com/example/arsitekturmvm/view/FormSiswa.kt
+++ app/src/main/java/com/example/arsitekturmvm/view/FormSiswa.kt
@@ -45,4 +45,21 @@ fun FormSiswa(
     var txtAlamat by remember {mutableStateOf("")}
     var txtGender by remember {mutableStateOf("")}
     val listData: MutableList<String> = mutableListOf(txtNama,txtGender,txtAlamat)
-}
+
+    Scaffold(modifier = Modifier,
+        {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors
+                    (containerColor = colorResource(id = R.color.teal_700))
+            )
+        }
+    ){
+        
+    }
+    }
