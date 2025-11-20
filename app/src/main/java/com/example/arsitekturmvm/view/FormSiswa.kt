813fc2a932ebb259fb14c172980f75dca462707a
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-11-20 16:51:06 +0700

    menambahkan outlinedtextfield untuk input nama

--- app/src/main/java/com/example/arsitekturmvm/view/FormSiswa.kt
+++ app/src/main/java/com/example/arsitekturmvm/view/FormSiswa.kt
@@ -59,7 +59,28 @@ fun FormSiswa(
                     (containerColor = colorResource(id = R.color.teal_700))
             )
         }
-    ){
-        
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = txtNama,
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = {
+                    txtNama = it
+                },
+            )
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(all = 20.dp)
+                    .width(width = 250.dp), thickness = Thickness, color = Color.Red
+            )
+        }
     }
-    }
+}
